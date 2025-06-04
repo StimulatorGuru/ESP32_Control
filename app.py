1633ed824e12a05a6bba4c08362ee5990cd9de86
StimulatorGuru <businesshub9921@gmail.com>
2025-06-04 10:50:42 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template
-from send_to_influx import write_control_frequency
+from send_to_influx import write_control_frequency, write_control_enable
 
 app = Flask(__name__)
 
@@ -18,6 +18,15 @@ def set_frequency():
             return "Frequency out of range (2-150 Hz)", 400
     except:
         return "Invalid frequency", 400
+
+@app.route("/toggle", methods=["GET"])
+def toggle_enable():
+    try:
+        enable = request.args.get("enable").lower() == "true"
+        write_control_enable(enable)
+        return f"Pulses {'enabled' if enable else 'disabled'}.", 200
+    except:
+        return "Invalid enable value", 400
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
